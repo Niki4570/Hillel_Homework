f28d9f8159a5c0a60717d9f815e001abcfd6c798
Niki4570 <nkrivoruchko125133@gmail.com>
2023-09-13 18:44:50 +0300

    Homework 10

---
+++ src/Java_HW10/Main.java
@@ -0,0 +1,10 @@
+package Java_HW10;
+
+import org.example.task_10.MavenExample;
+
+public class Main {
+    public static void main(String[] args){
+        MavenExample mavenExample = new MavenExample();
+        System.out.println(mavenExample.MathMethod(7,5));
+    }
+}
